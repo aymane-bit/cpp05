4919122c0b461ee6742d12827ce47b5fc6225035
Aymane Kajjou <akajjou@c3r6p1.1337.ma>
2025-05-14 00:00:39 +0100

    ok

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,14 +6,12 @@
 /*   By: akajjou <akajjou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 22:08:22 by akajjou           #+#    #+#             */
-/*   Updated: 2025/05/12 23:33:35 by akajjou          ###   ########.fr       */
+/*   Updated: 2025/05/13 23:30:50 by akajjou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "AForm.hpp"
 #include "Intern.hpp"
 
 int main()
@@ -21,11 +19,12 @@ int main()
     std::cout << "\n---------------Test with valaible Form Name---------------\n\n";
     Intern someRandomIntern;
     AForm* rrf;
-    rrf = someRandomIntern.makeForm("PresidentialPardonForm", "Bender");
+    rrf = someRandomIntern.makeForm("RobotomyRequestForm", "Bender");
+    std::cout << "\n---------------Test with a unvalaible Form Name---------------\n\n";
+    AForm *test;
+    test = someRandomIntern.makeForm("makaynch", "Bender");
+    std::cout << "\n---------------Test END-------------------------------\n";
     delete rrf;
-    // std::cout << "\n---------------Test with a unvalaible Form Name---------------\n\n";
-    // AForm * test;
-    // test = someRandomIntern.makeForm("makaynch", "Bender");
-
+    delete test;
     return 0;
 }
